276ecd3dd3abed1ac283a8f2b29184bbaf445fde
SeongHo-C <leeseong7130@gmail.com>
2024-11-28 15:43:06 +0900

    Refactor: 모델 수정 및 탐지 인수 추가

--- yolo8/detection.py
+++ yolo8/detection.py
@@ -3,14 +3,12 @@ import cv2
 import time
 
 def process_video_display(video_path):
-    model = YOLO("runs/detect/train/weights/best.pt")
+    model = YOLO("800-24-epoch163.pt")
     cap = cv2.VideoCapture(video_path)
     
     video_fps = int(cap.get(cv2.CAP_PROP_FPS))
     frame_delay = 1 / video_fps  
-    
-    print(f"원본 비디오 FPS: {video_fps}")
-    
+        
     try:
         while cap.isOpened():
             start_time = time.time()
@@ -19,7 +17,7 @@ def process_video_display(video_path):
             if not ret:
                 break
             
-            results = model(frame, verbose=False)
+            results = model(frame, verbose=False, conf=0.7, iou=0.5)
             annotated_frame = results[0].plot()
             
             processing_time = time.time() - start_time
@@ -35,10 +33,9 @@ def process_video_display(video_path):
             
             if cv2.waitKey(wait_time) & 0xFF == ord('q'):
                 break
-            
     finally:
         cap.release()
         cv2.destroyAllWindows()
 
-video_path = "../resource/youtube4.mp4"
+video_path = "../resource/youtube5.mp4"
 process_video_display(video_path)
